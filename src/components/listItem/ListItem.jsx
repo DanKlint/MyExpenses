fff7d1d59c05e388c1094d02a841bfbfb64c43a8
Daniil <klintcevd@mail.ru>
2023-08-26 14:29:00 +0400

    date format

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import "./ListItem.css";
 
 const ListItemImpl = ({ description, date, amount }) => {
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const formattedDate = formatDate(date);
+
   return (
     <li className="list-item">
       <div className="description">{description}</div>
-      <div className="date">{date}</div>
+      <div className="date">{formattedDate}</div>
       <div className="amount">{amount}₽</div>
     </li>
   );
